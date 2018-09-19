d25d1adc51c3b78a77d66a26f07cffd16307a5cd
LAPTOP_MP266 <LAPTOP_MP266@LAPTOP_MP266>
2018-09-19 11:06:08 +0530

    color

--- PartD/src/Information.java
+++ PartD/src/Information.java
@@ -4,7 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi, my name is Islam Beth");
-		
+		System.out.println("My favourite color is Red");
 	}
 
 }
