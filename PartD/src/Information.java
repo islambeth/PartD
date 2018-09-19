9e8a28eac326f14ce781180e1d682de72abe2d27
LAPTOP_MP266 <LAPTOP_MP266@LAPTOP_MP266>
2018-09-19 11:09:18 +0530

    transportation update

--- PartD/src/Information.java
+++ PartD/src/Information.java
@@ -5,6 +5,7 @@ public class Information {
 		
 		System.out.println("Hi, my name is Islam Beth");
 		System.out.println("My favourite color is Red");
+		System.out.println("My favourite mode of transportation is Bus");
 	}
 
 }
